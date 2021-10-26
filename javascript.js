1df3d206c630d315d997b9abc1d7e02bce202c04
Wellyngton29 <89717569+Wellyngton29@users.noreply.github.com>
2021-10-26 16:04:09 -0300

    primeiro commit

---
+++ javascript.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+let inputidade = document.getElementsByTagName("input")[0]
+let inputpeso = document.getElementsByTagName("input")[1]
+let inputaltura = document.getElementsByTagName("input")[2]
+
+
+
+
+
+
+function caucular() {
+
+    let selected = document.getElementById("valor").value
+    let textarea = document.getElementsByTagName("textarea")[0]
+    let peso = inputpeso.value
+    let altura = inputaltura.value
+    let idade = inputidade.value
+
+    let valortotal
+
+    if (selected === "Homem") {
+
+         valortotal = (66 + (13.8 * peso) + (5.0 * altura) - (6.8 * idade))
+
+    }
+    if (selected === "Mulher") {
+         valortotal = (655 + (9.6 * peso) + (1.9 * altura) - (4.7 * idade))
+    }
+    if(valortotal === 66 ){
+        valortotal = 0
+    }
+    if(valortotal === 655){
+        valortotal = 0
+    }
+    textarea.innerHTML = "voce consumiu " + valortotal.toFixed(0) + " calorias"
+}
+
+
