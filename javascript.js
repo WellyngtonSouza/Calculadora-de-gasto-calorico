6bac013058e9122ccfc9492fe77512f93005125b
Wellyngton29 <89717569+WellyngtonSouza@users.noreply.github.com>
2021-12-23 13:44:43 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,10 +3,9 @@ let inputidade = document.getElementsByTagName("input")[0]
 let inputpeso = document.getElementsByTagName("input")[1]
 let inputaltura = document.getElementsByTagName("input")[2]
 
-function iniciar(){
+function iniciar() {
     let imagem1 = document.getElementsByTagName("img")[0]
     imagem1.style.backgroundColor = "blue"
-
 }
 
 function caucular() {
@@ -19,38 +18,49 @@ function caucular() {
 
     let valortotal
 
-    if (selected === "Homem") {
+    if (inputidade.value == 0 && inputaltura.value == 0 && inputpeso.value == 0) {
+        alert("por favor, preencha os dados")
+        textarea.innerHTML = "preencha os dados"
+    }
 
-         valortotal = (66 + (13.8 * peso) + (5.0 * altura) - (6.8 * idade))
+    else {
+        if (inputpeso.value == 0) {
+            alert("voce precisa preencher a peso")
+        }
+        if (inputidade.value == 0) {
+            alert("voce precisa preencher a idade")
+        }
+        if (inputaltura.value == 0) {
+            alert("voce precisa preencher a altura")
+        }
+        if (selected === "Homem") {
+
+            valortotal = (66 + (13.8 * peso) + (5.0 * altura) - (6.8 * idade))
+        }
+        if (selected === "Mulher") {
+            valortotal = (655 + (9.6 * peso) + (1.9 * altura) - (4.7 * idade))
+        }
+
+        textarea.innerHTML = "você consumiu " + valortotal.toFixed(0) + " calorias"
 
     }
-    if (selected === "Mulher") {
-         valortotal = (655 + (9.6 * peso) + (1.9 * altura) - (4.7 * idade))
-    }
-    if(valortotal === 66 ){
-        valortotal = 0
-    }
-    if(valortotal === 655){
-        valortotal = 0
-    }
-    textarea.innerHTML = "você consumiu " + valortotal.toFixed(0) + " calorias"
 }
-function mudar(){
+function mudar() {
     let selected = document.getElementById("valor").value
     let imagem1 = document.getElementsByTagName("img")[0]
     let imagem2 = document.getElementsByTagName("img")[1]
 
-    if(selected === "Homem"){
+    if (selected === "Homem") {
         imagem1.style.backgroundColor = "blue"
         imagem2.style.backgroundColor = "white"
     }
-   
-    if(selected === "Mulher"){
+
+    if (selected === "Mulher") {
         imagem2.style.backgroundColor = "#ff6781"
         imagem1.style.backgroundColor = "white"
     }
 
-    
+
 }
 
 
